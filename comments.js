569fdda3b4c60783180700988b7f0234fe0ff449
Marcelo Toledo <marcelotoledo5000@gmail.com>
2023-12-12 18:05:51 -0300

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,19 @@
+// Create web server
+
+// Import modules
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+// Import models
+const Post = require('../models/post');
+const Comment = require('../models/comment');
+
+// Create comment
+router.post('/', (req, res, next) => {
+  // Create comment object
+  const comment = new Comment({
+    _id: new mongoose.Types.ObjectId(),
+    content: req.body.content,
+    )
+});
