37506922828315942b8ee607cd97f18ec65459e4
Marcelo Toledo <marcelotoledo5000@gmail.com>
2024-01-04 20:51:06 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,15 +1,12 @@
 function skillsMember() {
-  return {
-    restrict: 'E',
-    templateUrl: 'templates/skills-member.html',
-    scope: {
-      member: '='
-    },
-    controller: function($scope) {
-      $scope.show = false;
-      $scope.toggle = function() {
-        $scope.show = !$scope.show;
-      };
+    return {
+        restrict: 'E',
+        templateUrl: 'app/components/member/member.html',
+        controller: 'MemberController',
+        controllerAs: 'vm',
+        bindToController: true,
+        scope: {
+            member: '='
+        }
     }
-  };
 }
